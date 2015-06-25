3a1197217d59852d75e8b9fa1a06d4c206dbc295
kai <kyo6728@gmail.com>
2015-06-24 21:48:28 -0400

    login, logout, getJWT, refreshJWT, accessRestricted

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -8,19 +8,71 @@ angular.module('starter.controllers', [])
 	$scope.login = function() {
 		console.log( "LOGIN USER: " + $scope.data.username + " - PW: " + $scope.data.password );
 		
-		AuthService.login($scope.data.username, $scope.data.password).then( function(d){
-			$scope.jwt = d.data;
-			console.log( "$scope.jwt: " + angular.toJson($scope.jwt) );
-		});
+		AuthService.login($scope.data.username, $scope.data.password)
+		.then( function(d){
+				$scope.interpages_msg.jwt = angular.toJson(d.data);
+				console.log( "$scope.interpages_msg.jwt in DashCtrl: " + $scope.interpages_msg.jwt );
+				$scope.jwt = angular.toJson(d.data);
+				console.log( "$scope.jwt in DashCtrl: " + $scope.jwt );
+			}, function(d){
+				$scope.jwt = angular.toJson(d.data);
+				$ionicPopup.alert({
+					title: 'Login Failed!',
+					template: 'Incorrect credentials',
+				});
+				console.log( "AuthService.login() ERROR in DashCtrl: " + angular.toJson(d.data) );
+			});
 		
+	}
+
+	$scope.logout = function() {
+		AuthService.logout();
+		$scope.interpages_msg.jwt = AuthService.getJWT();
+		$scope.jwt = AuthService.getJWT();
 	}
 
 })
 
-.controller('ChatsCtrl', function($scope, AuthService, INTERPAGES_MSG) {
+.controller('ChatsCtrl', function($scope, AuthService, $state, $ionicPopup, INTERPAGES_MSG) {
 	$scope.interpages_msg = INTERPAGES_MSG;
-	$scope.jwt = AuthService.getJWT();
+	
+	$scope.getToken = function() {
+		$scope.jwt = AuthService.getJWT();
+		console.log( "$scope.jwt in ChatsCtrl: " + angular.toJson($scope.jwt) );
+	}
+	
+	$scope.refreshToken = function() {
+		AuthService.refreshJWT()
+		.then( function(d){
+				$scope.newjwt = angular.toJson(d.data);
+				console.log( "$scope.newjwt in ChatsCtrl: " + angular.toJson($scope.newjwt) );
+			}, function(d){
+				$scope.newjwt = angular.toJson(d.data);
+				console.log( "AuthService.refreshJWT() ERROR in ChatsCtrl: " + angular.toJson(d.data) );
+				
+				$ionicPopup.alert({
+					title: 'Session expired!',
+					template: 'Please login',
+				}).then( function(){
+					$state.go('tab.dash'); // login page
+				});
+			});
+	}
+
 })
+
+.controller('AccountCtrl', function($scope, AuthService) {
+	$scope.accessRestricted = function() {
+		AuthService.accessRestricted()
+		.then( function(d) {
+			$scope.returndata = angular.toJson(d.data);
+			console.log( "$scope.returndata in AccountCtrl: " + $scope.returndata );
+		}, function(d) {
+			$scope.returndata = angular.toJson(d.data);
+			console.log( "AuthService.accessRestricted() ERROR in AccountCtrl: " + $scope.returndata );
+		});
+	}
+}); // last controller
 
 /*
 .controller('ChatsCtrl', function($scope, Chats) {
@@ -37,7 +89,6 @@ angular.module('starter.controllers', [])
     Chats.remove(chat);
   }
 })
-*/
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
@@ -48,3 +99,6 @@ angular.module('starter.controllers', [])
     enableFriends: true
   };
 });
+*/
+
+
--- myApp/www/js/services.js
+++ myApp/www/js/services.js
@@ -2,9 +2,6 @@ angular.module('starter.services', [])
 
 .factory('INTERPAGES_MSG', function() { // global data share between pages
 	interpages_msg = {};
-	//interpages_msg.msg = "";
-	//interpages_msg.token = "";
-	//interpages_msg.status = "";
 	return interpages_msg;
 })
 
@@ -17,12 +14,18 @@ angular.module('starter.services', [])
 		get: function(key, defaultValue) {
 			return $window.localStorage[key] || defaultValue;
 		},
+		remove: function(key) {
+		    $window.localStorage.removeItem(key);
+		},
 		setObject: function(key, value) {
 			$window.localStorage[key] = JSON.stringify(value);
 		},
 		getObject: function(key) {
 		    return JSON.parse($window.localStorage[key] || '{}');
-		}
+		},
+		removeObject: function(key) {
+		    $window.localStorage.removeItem(key);
+		},
 	}
 }])
 
@@ -80,9 +83,10 @@ angular.module('starter.services', [])
 
 var AuthService = angular.module('AuthService', []);
 
-AuthService.factory('AuthService', ['$http', function($http) {
-	var url = "http://127.0.0.1:8000/token-demo/api-token-auth/";
-	var res = [];
+AuthService.factory('AuthService', ['$http', '$localstorage', function($http, $localstorage) {
+	var url_auth = "http://127.0.0.1:8000/token-demo/api-token-auth/";
+	var url_refresh = "http://127.0.0.1:8000/token-demo/api-token-refresh/";
+	var url_restricted = "http://127.0.0.1:8000/token-demo/users-restricted/";
 
 	return {
 		login: function(username, password){
@@ -101,13 +105,13 @@ AuthService.factory('AuthService', ['$http', function($http) {
 			};
 
 			var ret = $http({
-				url: url,
+				url: url_auth,
 				method: "POST",
 				headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
 				data: Object.toParams(creds),
 			})
 			.success( function(response){
-				res = response;
+				$localstorage.setObject('jwt', response);
 				console.log("login() in AuthService success: " + angular.toJson(response) );
 				return response;
 			}).error( function(response){
@@ -117,15 +121,54 @@ AuthService.factory('AuthService', ['$http', function($http) {
 			return ret;
 		},
 		
+		logout: function() {
+			return $localstorage.removeObject('jwt');
+		},
+		
 		getJWT: function() {
-			return jwt;		
+			return $localstorage.getObject('jwt');	
 		},
 		
 		refreshJWT: function() {
-			return jwt;
+			var $jwt = $localstorage.getObject('jwt');
+			
+			var ret = $http({
+				url: url_refresh,
+				method: "POST",
+				headers: {'Content-Type': 'application/json'},
+				data: $jwt,
+			})
+			.success( function(response){
+				$localstorage.setObject('jwt', response);
+				console.log("refreshJWT() in AuthService success: " + angular.toJson(response) );
+				return response;
+			}).error( function(response){
+				console.log("refreshJWT() in AuthService error: " + angular.toJson(response) );
+			});
+			
+			return ret;
+		},
+		
+		accessRestricted: function() {
+			var $jwt = $localstorage.getObject('jwt');
+			//console.log("accessRestricted() $jwt: " + angular.toJson($jwt.token) );
+			var ret = $http({
+				url: url_restricted,
+				method: "GET",
+				headers: {'Authorization': 'JWT ' + $jwt.token},
+			})
+			.success( function(response){
+				//$localstorage.setObject('jwt', response);
+				console.log("accessRestricted() in AuthService success: " + angular.toJson(response) );
+				return response;
+			}).error( function(response){
+				console.log("accessRestricted() in AuthService error: " + angular.toJson(response) );
+			});
+			
+			return ret;
 		}
+		
 	}// return
-	
 }]);
 
 
